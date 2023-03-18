fef39667b7ba2208d2afe11746701712df341d92
Minha Kwon <namisoft@naver.com>
2023-03-18 14:21:14 +0900

    headerBack 컴포넌트 추가

---
+++ src/components/HeaderBack.js
@@ -0,0 +1,29 @@
+import { View, Text } from "react-native";
+import React from "react";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+const Container = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px 14px;
+`;
+
+const HistoryBack = styled.TouchableOpacity`
+  padding: 4px;
+  align-items: center;
+`;
+
+const HeaderBack = () => {
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <HistoryBack onPress={() => navigation.pop()}>
+        <Ionicons name="chevron-back-outline" size={28} color="#d2dae2" />
+      </HistoryBack>
+    </Container>
+  );
+};
+
+export default HeaderBack;
--- src/screens/TotalMenu.js
+++ src/screens/TotalMenu.js
@@ -1,35 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
 import styled from "styled-components/native";
-import { Ionicons } from "@expo/vector-icons";
 import Container from "../components/Container";
 import Background from "../components/Background";
+import HeaderBack from "../components/HeaderBack";
 
 const GoPage = styled.TouchableOpacity`
   padding: 20px;
   background-color: red;
 `;
 
-const Header = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 10px 14px;
-`;
-
-const HistoryBack = styled.TouchableOpacity`
-  padding: 4px;
-  align-items: center;
-`;
-
-const TotalMenu = ({ navigation: { pop } }) => {
+const TotalMenu = () => {
   return (
     <Container>
       <Background />
-      <Header>
-        <HistoryBack onPress={() => pop()}>
-          <Ionicons name="chevron-back-outline" size={28} color="#d2dae2" />
-        </HistoryBack>
-      </Header>
+      <HeaderBack />
       <GoPage>
         <Text>go back</Text>
       </GoPage>
