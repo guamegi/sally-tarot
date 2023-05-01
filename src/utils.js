46836c8b75e88f3ae01c2bc0733fb2e6a6459d8f
Minha Kwon <namisoft@naver.com>
2023-05-01 09:25:05 +0900

    광고 id 변경

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const InterstitialAdAppId = {
-  android: "ca-app-pub-7307128452071420/4712330038",
+  android: "ca-app-pub-7307128452071420/1234210876",
   ios: "ca-app-pub-7307128452071420/2971587541",
 };
